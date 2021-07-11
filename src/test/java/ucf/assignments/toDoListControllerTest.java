826aa99ca4b1d74e970b7961ca7bcf1b0e948cd4
jasonsaini <jasonsaini.exe@gmail.com>
2021-07-11 14:29:06 -0400

    Updated unit tests for assignment requirements

--- src/test/java/ucf/assignments/toDoListControllerTest.java
+++ src/test/java/ucf/assignments/toDoListControllerTest.java
@@ -64,10 +64,190 @@ class toDoListControllerTest {
         // pull array from JSON object
         JSONObject currentObject = (JSONObject)obj;
         JSONArray itemsList = (JSONArray)currentObject.get("items");
-        // assert that number of items in json list are equal to
+
+        // assert that number of items in json list are equal to expected
         int actual = itemsList.size();
         assertEquals(expected,actual);
 
     }
+
+    @Test
+    void descriptionMeetsCapacity()
+    {
+        // in gui, add a task with a description field of 256
+        // test string:
+        //9ctZXIQDTDFIgkywaFmIDbQ0HsWlAi9SGlWmycBbTqCFlTarVSIHKqMzZ8AXXUEq2FasddspDrTZR00VU9mu3vVAQThcyPyDiXIPEIqn8rOH6fH84IWyngJlXjknKvmGUVy8sGQgtBQxKXMgGo6gJEUbiMrwTvxQCPWlMT7adD2PiElcAjt6m1Nnk9j7rZYehwi87W2g5M0rSyp551QPZEuy9782DwxftDZZlV2PcUgaARA6tk9OWK1tWqsw5qpK
+
+        // initialize test directory
+        String testDir = "src/test/java/ucf/assignments/test_files/descriptionTest1.json";
+
+        // retrieve file with description test obejct
+        File testFile = new File(testDir);
+        Object obj = new Object();
+        try {
+            obj = new JSONParser().parse(new FileReader(testFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        // pull string from JSON object
+        JSONObject currentObject = (JSONObject)obj;
+        JSONArray itemsList = (JSONArray)currentObject.get("items");
+        JSONObject itemObj = (JSONObject)itemsList.get(0);
+        String descriptionString = (String)itemObj.get("description");
+
+        // asserts that description string meets minimum capacity
+        int expected = 256;
+        int actual = descriptionString.length();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void validDueDate()
+    {
+
+    }
+
+    @Test
+    void removesItem()
+    {
+        // in gui, populate to-do list with one item.
+        // remove the item using right-click context menu
+
+        // retrieve file from test_files directory
+        // initialize test directory
+        String testDir = "src/test/java/ucf/assignments/test_files/removeItemTest1.json";
+
+        // retrieve file with description test obejct
+        File testFile = new File(testDir);
+        Object obj = new Object();
+        try {
+            obj = new JSONParser().parse(new FileReader(testFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        // collect array of items from test file
+        JSONObject currentObject = (JSONObject)obj;
+        JSONArray itemsList = (JSONArray)currentObject.get("items");
+        // assert that list was empty after removing single item
+        int expected = 0;
+        int actual = itemsList.size();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void clearsList()
+    {
+        // in gui, populate to-do list with multiple items.
+        // clear the list via menubar
+
+        // retrieve file from test_files directory
+        // initialize test directory
+        String testDir = "src/test/java/ucf/assignments/test_files/clearListTest1.json";
+
+        // retrieve file with description test obejct
+        File testFile = new File(testDir);
+        Object obj = new Object();
+        try {
+            obj = new JSONParser().parse(new FileReader(testFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        // collect array of items from test file
+        JSONObject currentObject = (JSONObject)obj;
+        JSONArray itemsList = (JSONArray)currentObject.get("items");
+        // assert that list was empty after removing single item
+        int expected = 0;
+        int actual = itemsList.size();
+
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    void editsDescription()
+    {
+        // retrieve file from test_files directory
+        // initialize test directory
+        String testDir = "src/test/java/ucf/assignments/test_files/editsDescriptionTest1.json";
+
+        // retrieve file with description test obejct
+        File testFile = new File(testDir);
+        Object obj = new Object();
+        try {
+            obj = new JSONParser().parse(new FileReader(testFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        // pull string from JSON object
+        JSONObject currentObject = (JSONObject)obj;
+        JSONArray itemsList = (JSONArray)currentObject.get("items");
+        JSONObject itemObj = (JSONObject)itemsList.get(0);
+
+        // this is the string before editing the object
+        String originalDescription = "test1";
+
+
+        // In gui, load list & edit the description of the pre-existing item.
+        String newDescription = (String)itemObj.get("description");
+        boolean expected = true;
+        boolean actual = !originalDescription.equals(newDescription);
+        // if the old description is not equal to new, edit worked
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void editsDueDate()
+    {
+
+    }
+
+    @Test
+    void marksComplete()
+    {
+
+    }
+
+    @Test
+    void marksIncomplete()
+    {
+
+    }
+
+    @Test
+    void filtersComplete()
+    {
+
+    }
+
+    @Test
+    void filtersIncomplete()
+    {
+
+    }
+
+    @Test
+    void savesList()
+    {
+
+    }
+
+    @Test
+    void loadsList()
+    {
+
+    }
+
+
+
 
 }
